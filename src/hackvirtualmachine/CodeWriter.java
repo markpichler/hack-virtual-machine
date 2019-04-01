e6eea2db0c833dc32417e299105ef3f35bde0c90
Mark Pichler <mark_pichler@yahoo.com>
2019-04-01 13:03:20 -0700

    Fix and complete Push functionality of CodeWriter

--- src/hackvirtualmachine/CodeWriter.java
+++ src/hackvirtualmachine/CodeWriter.java
@@ -2,7 +2,6 @@ package hackvirtualmachine;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,40 +10,54 @@ import hackvirtualmachine.Parser.CommandType;
 public class CodeWriter {
 
     private PrintWriter outputFile;
-    private List<String> output;
     private List<String> segmentList;
     private int segmentID;
+    private String staticFile;
     private final String PUSH =
             "@SP\n" +
             "AM=M+1\n" +
             "A=A-1\n" +
             "M=";
+    private final String POP =
+            "@SP\n" +
+            "AM=M-1\n" +
+            "D=M";
 
     public CodeWriter(String fileName) {
         try {
-            outputFile = new PrintWriter(fileName + ".asm");
+            outputFile = new PrintWriter(fileName.substring(0,
+                    fileName.lastIndexOf(".")) + ".asm");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(0);
         }
-        output = new ArrayList<>();
+
         segmentList = Arrays.asList("argument", "local", "this", "that",
-                "pointer", "temp");
+                "pointer", "temp", "static");
+        staticFile = fileName.substring(fileName.lastIndexOf("/") + 1,
+                fileName.length() - 2);
     }
     public void writePushPop(CommandType command, String segment, int index) {
         switch (command) {
             case C_PUSH:
                 if (segment.equals("constant")) {
-                    if (index < -1 || index > 1) {
-                        output.add(PUSH + index);
+                    if (index > -1 && index < 1) {
+                        outputFile.println(PUSH + index);
                     } else {
-                        output.add(
+                        outputFile.println(
                                 "@" + index + "\n" +
                                 "D=A\n" +
                                 PUSH + "D");
                     }
+                } else if (segment.equals("static")) {
+                    outputFile.println(
+                            "@" + staticFile + index + "\n" +
+                            "D=M\n" +
+                            PUSH + "D");
                 } else if (segmentList.contains(segment)) {
+
                     segmentID = segmentList.indexOf(segment);
+
                     switch (segmentID) {
                         case 0:
                             segment = "ARG";
@@ -64,20 +77,27 @@ public class CodeWriter {
                         case 5:
                             segment = "R" + (5 + index);
                     }
-                    if (index == 0) {
-                        output.add(
+
+                    // Temp and Pointer give exact address to access.
+                    if (segmentID == 5 || segmentID == 4) {
+                        outputFile.println(
+                                "@" + segment + "\n" +
+                                "D=M\n" +
+                                PUSH + "D");
+                    } else if (index == 0) {
+                        outputFile.println(
                                 "@" + segment + "\n" +
                                 "A=M\n" +
                                 "D=M\n" +
                                 PUSH + "D");
                     } else if (index == 1) {
-                        output.add(
+                        outputFile.println(
                                 "@" + segment + "\n" +
                                 "A=M+1\n" +
                                 "D=M\n" +
                                 PUSH + "D");
                     } else {
-                        output.add(
+                        outputFile.println(
                                 "@" + index + "\n" +
                                 "D=A\n" +
                                 "@" + segment + "\n" +
